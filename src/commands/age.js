4f56551e5c58b9a96489d960666d7f287819adb8
Tim <butterstim@yahoo.com>
2020-09-03 23:02:14 -0400

    pg

--- src/commands/age.js
+++ src/commands/age.js
@@ -9,49 +9,9 @@ module.exports.run = async(client, message, args) => {
     const response = await age_guess.json();
 
     if (response) {
-        message.reply(`is probably ${response.age}. ${age_message(response.age)}`);
+        message.reply(`is probably ${response.age}.`);
     } else {
         message.reply(`your name is whack, idk.`);
     }
 }
 
-
-// CHOOSE A MESSAGE BASED ON A PERSON'S GUESSED AGE
-function age_message (age) {
-    let msg;
-    if (age && age > 0) {
-        switch(true) {
-            case (age < 6):
-                msg = "Stop crying about Sunday school with pastor Tom, he's a nice man.";
-                break;
-            case(age < 12):
-                msg = "Puberty is soon! Start stocking up on acne wash.";
-                break;
-            case(age < 16):
-                msg = "You can't even drive lol get gud.";
-                break;
-            case(age < 21):
-                msg = "You're old enough for war, but not a war with your liver. Go buy a Juul instead, that's cooler with the kids these days anyways.";
-                break;
-            case(age < 30):
-                msg = "Don't settle down yet, you've still got a lot of partying to do.";
-                break;
-            case(age < 40):
-                msg = "You're either rotting in a routine or shagging your way around.";
-                break;
-            case(age < 50):
-                msg = "Over the hill with you!";
-                break;
-            case(age < 60):
-                msg = "Hey! You're eligible to join the AARP!";
-                break;
-            case(age < 70):
-                msg = "Wear a mask and avoid the outside world.";
-                break;
-            default:
-                msg = "Enjoy your remaining years!";
-                break;
-        }
-    }
-    return msg;
-}
